bcecc1fa2e798f9787b50def6d25b56918624c21
Mohamed khidir ageeb <Mohamed khidir ageeb@DESKTOP-BPDC872>
2018-11-25 10:50:38 +0300

    Edit ValuesController

--- WebApi_Test/Controllers/ValuesController.cs
+++ WebApi_Test/Controllers/ValuesController.cs
@@ -4,37 +4,57 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Results;
+using System.Web.Http;
+using System.Web.Mvc;
 
 namespace WebApi_Test.Controllers
 {
-    [Authorize]
+   // [Authorize]
     public class ValuesController : ApiController
     {
+
+        static List<string> Names = new List<string>()
+        {
+            "Abdulla",
+            "Mohamed",
+            "Saleh",
+            "Omer"
+
+        };
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return Names;
         }
 
         // GET api/values/5
         public string Get(int id)
         {
-            return "value";
+            return Names[id];
         }
 
         // POST api/values
         public void Post([FromBody]string value)
         {
+            Names.Add(value);
+            
         }
 
         // PUT api/values/5
         public void Put(int id, [FromBody]string value)
         {
+
+            Names[id] = value;
+          
         }
 
         // DELETE api/values/5
         public void Delete(int id)
         {
+            Names.RemoveAt(id);
+
+          
         }
     }
 }
